091319d98368d8ca76b7af0397c9c55c5fbd83f2
Ido Lempert <ido.lempert@poalim.co.il>
2019-04-22 16:05:23 +0300

    add basic logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Card} from './card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'memory-game';
+export class AppComponent implements OnInit {
+  cards: Card[];
+
+  private flipedCard: Card;
+
+  ngOnInit(): void {
+    this.shuffle();
+  }
+
+  flip(card: Card) {
+    if (card.flipped) {
+      return;
+    }
+
+    card.flipped = true;
+    if (this.flipedCard) {
+      if (this.flipedCard.frontImageUrl === card.frontImageUrl) {
+        // TODO: check finish
+        this.flipedCard = undefined;
+      } else {
+        setTimeout(() => {
+          // TODO :: fix race condition
+          this.flipedCard.flipped = card.flipped = false;
+          this.flipedCard = undefined;
+        }, 300);
+      }
+    } else {
+      this.flipedCard = card;
+    }
+  }
+
+  shuffle() {
+    this.cards = [];
+
+    for (let i = 1; i <= 5; i++) {
+      for (let j = 0; j < 2; j++) {
+        const card = {
+          flipped: false,
+          backImageUrl: 'assets/images/back.jpg',
+          frontImageUrl: `assets/images/${ i < 10 ? '0' + i : i}.jpg`
+        };
+        this.cards.push(card);
+      }
+    }
+
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]; // swap elements
+    }
+  }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, HostBinding, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Card} from '../card';
+import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnChanges {
+  @Input() private card: Card;
+  @HostBinding('class.flipped') @Input() private flipped = false;
+  frontImageUrl: SafeStyle;
+  backImageUrl: SafeStyle;
+  constructor(private sanitizer: DomSanitizer) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    this.flipped = this.card.flipped;
+    this.frontImageUrl = this.sanitizer.bypassSecurityTrustStyle(`url(${this.card.frontImageUrl})`);
+    this.backImageUrl = this.sanitizer.bypassSecurityTrustStyle(`url(${this.card.backImageUrl})`);
   }
 
 }
