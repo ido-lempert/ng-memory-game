0390768bcaa99db37a6be40648ca55ca576b454f
Ido Lempert <ido.lempert@poalim.co.il>
2019-04-28 16:51:31 +0300

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import { FinishBannerComponent } from './finish-banner/finish-banner.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { FinishBannerComponent } from './finish-banner/finish-banner.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
